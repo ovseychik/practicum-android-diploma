f9c0a30601e86f2996c9cedbd93deaa50de6c2f8
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-20 14:25:34 +0300

    - создал модели Request и константы. Константа HH_USER_AGENT для заголовка

---
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/SearchRequest.kt
@@ -0,0 +1,19 @@
+package ru.practicum.android.diploma.data.dto
+
+const val HH_USER_AGENT: String = "HH-User-Agent: Get a job! (morozov@rtr.spb.ru)"
+
+const val KEY_PAGE = "page"
+const val KEY_PER_PAGE = "per_page"
+const val KEY_TEXT = "text"
+const val KEY_AREA = "area"
+const val KEY_INDUSTRY = "industry"
+
+data class SearchRequest(
+    val searchOptions: MutableMap<String, String> = mutableMapOf(
+        KEY_PAGE to "",
+        KEY_PER_PAGE to "",
+        KEY_TEXT to "",
+        KEY_AREA to "",
+        KEY_INDUSTRY to ""
+    ),
+)
