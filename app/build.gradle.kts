635f49fa40d34389ca74bd7970ec48ac8bfa5cfc
Mark Quarasique <julipaha@gmail.com>
2024-01-20 09:16:47 +0300

    Добавляет viewBinding и kotlin-kapt в зависимости

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("ru.practicum.android.diploma.plugins.developproperties")
+    id("kotlin-kapt")
 }
 
 android {
@@ -36,6 +37,7 @@ android {
 
     buildFeatures {
         buildConfig = true
+        viewBinding = true
     }
 }
 
