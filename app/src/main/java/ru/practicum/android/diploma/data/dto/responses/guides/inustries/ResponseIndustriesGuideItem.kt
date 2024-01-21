664bd1f35bab8d00b210b85e6d7af646c2282c8a
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-22 01:07:41 +0300

    - переименовал

--- app/src/main/java/ru/practicum/android/diploma/data/dto/responses/guides/inustries/ResponseIndustriesGuideItem.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/responses/guides/inustries/ResponseIndustriesGuideItem.kt
@@ -1,6 +1,6 @@
 package ru.practicum.android.diploma.data.dto.responses.guides.inustries
 
-data class ResponseGuideIndustriesDtoItem(
+data class ResponseIndustriesGuideItem(
     val id: String,
     val industries: List<Industry>, // список подиндустрий
     val name: String // название индустрии
