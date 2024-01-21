3e9e59723cbc1c28a32c8d409e0697634f33591e
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-21 21:10:54 +0300

    - написал модельку-обертку для domain

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/SearchResultData.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.domain.models
+
+sealed interface SearchResultData<T> {
+    data class Data<T>(val value: T) : SearchResultData<T>
+    data class ErrorServer<T>(val message: String) : SearchResultData<T>
+    data class NoInternet<T>(val message: String) : SearchResultData<T>
+}
