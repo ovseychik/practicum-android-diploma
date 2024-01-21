271a950fb56a1566f51812d66975b38bace84775
Aslan <shkahov91@gmail.com>
2024-01-22 01:26:58 +0300

    поправил VacancyEntity.kt, поправил опечатку в названии DomainModule.kt

--- app/src/main/java/ru/practicum/android/diploma/data/db/entity/VacancyEntity.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/entity/VacancyEntity.kt
@@ -9,36 +9,44 @@ data class VacancyEntity(
     @PrimaryKey
     @ColumnInfo(name = "id")
     val id: String,
-    @ColumnInfo(name = "name")
-    val name: String?,
-    @ColumnInfo(name = "currency")
-    val currency: String?,
-    @ColumnInfo(name = "salaryFrom")
-    val salaryFrom: Int?,
-    @ColumnInfo(name = "salaryTo")
-    val salaryTo: Int?,
-    @ColumnInfo(name = "gross")
-    val gross: Boolean?,
     @ColumnInfo(name = "url")
     val url: String?,
+    @ColumnInfo(name = "name")
+    val name: String?,
+    @ColumnInfo(name = "salaryFrom")
+    val salaryFrom: String?,
+    @ColumnInfo(name = "salaryTo")
+    val salaryTo: String?,
+    @ColumnInfo(name = "currency")
+    val currency: String?,
+    @ColumnInfo(name = "gross")
+    val gross: Boolean?,
+    @ColumnInfo(name = "employerId")
+    val employerId: String?,
     @ColumnInfo(name = "area")
     val area: String?,
+    @ColumnInfo(name = "experienceId")
+    val experienceId: String?,
+    @ColumnInfo(name = "employment")
+    val employment: String?,
+    @ColumnInfo(name = "schedule")
+    val schedule: String?,
+    @ColumnInfo(name = "contactPerson")
+    val contactPerson: String?,
     @ColumnInfo(name = "email")
     val email: String?,
     @ColumnInfo(name = "phone")
     val phone: String?,
     @ColumnInfo(name = "description")
     val description: String?,
-    @ColumnInfo(name = "employerId")
-    val employerId: String?,
-    @ColumnInfo(name = "experienceId")
-    val experienceId: String?,
     @ColumnInfo(name = "keySkills")
     val keySkills: String?,
     @ColumnInfo(name = "professionalRoles")
     val professionalRoles: String?,
     @ColumnInfo(name = "publishedAt")
     val publishedAt: String?,
+    @ColumnInfo(name = "comment")
+    val comment: String?,
 ) {
     companion object {
         const val TABLE_NAME = "vacancy_table"
