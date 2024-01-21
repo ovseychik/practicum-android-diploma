9ffe672621955f9e3bb51f3e200174f18820d32a
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-22 01:13:30 +0300

    - добавил колличество записей на странице с сайта

--- app/src/main/java/ru/practicum/android/diploma/data/dto/SearchRequest.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/SearchRequest.kt
@@ -7,11 +7,12 @@ const val KEY_PER_PAGE = "per_page"
 const val KEY_TEXT = "text"
 const val KEY_AREA = "area"
 const val KEY_INDUSTRY = "industry"
+const val PAGES_PER_SHEET = "20"
 
 data class SearchRequest(
     val searchOptions: MutableMap<String, String> = mutableMapOf(
         KEY_PAGE to "",
-        KEY_PER_PAGE to "",
+        KEY_PER_PAGE to PAGES_PER_SHEET,
         KEY_TEXT to "",
         KEY_AREA to "",
         KEY_INDUSTRY to ""
