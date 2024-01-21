d74260e66cd6d8b5ca5dda1ac3d3f947b2c92481
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-21 21:09:27 +0300

    - добавил зависимости в dataModule

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,5 +1,36 @@
 package ru.practicum.android.diploma.di
 
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.data.NetworkClient
+import ru.practicum.android.diploma.data.network.HHApi
+import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
 
-val dataModule = module { }
+private const val SETTINGS_APP = "settings_app"
+val dataModule = module {
+    single<HHApi> {
+        Retrofit.Builder()
+            .baseUrl("https://api.hh.ru")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(HHApi::class.java)
+    }
+
+    single<NetworkClient> {
+        RetrofitNetworkClient(hhService = get(), androidContext())
+    }
+
+    factory { Gson() }
+
+    single<SharedPreferences> {
+        androidContext().getSharedPreferences(
+            SETTINGS_APP,
+            AppCompatActivity.MODE_PRIVATE
+        )
+    }
+}
