e2e54e3dc2c02586fdfc4147ad430fe73c53202e
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-22 10:39:30 +0300

    - убрал часть констант - использую константы HttpURLConnection

--- app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
@@ -11,100 +11,102 @@ import ru.practicum.android.diploma.data.dto.responses.Response
 import ru.practicum.android.diploma.data.dto.responses.ResponseGuide
 import ru.practicum.android.diploma.data.dto.responses.guides.countries.ResponseCountriesGuideItem
 import ru.practicum.android.diploma.data.dto.responses.guides.inustries.ResponseIndustriesGuideItem
+import ru.practicum.android.diploma.util.GlobalConstant
 import ru.practicum.android.diploma.util.isConnected
-
-private const val NO_INTERNET = -1
-private const val VALID_RESPONSE = 200
-private const val SERVER_ERROR = 500
+import java.net.HttpURLConnection
 
 class RetrofitNetworkClient(private val hhService: HHApi, private val context: Context) : NetworkClient {
     override suspend fun getVacancies(request: SearchRequest): Response {
-        if (isConnected(context) == false) {
-            return Response().apply { resultCode = NO_INTERNET }
+        if (!isConnected(context)) {
+            return Response().apply { resultCode = GlobalConstant.NO_INTERNET }
         }
         val response = withContext(Dispatchers.IO) {
             try {
                 val result = hhService.getVacancies(request.searchOptions)
-                result.apply { resultCode = VALID_RESPONSE }
+                result.apply { resultCode = HttpURLConnection.HTTP_OK }
             } catch (e: HttpException) {
-                Response().apply { resultCode = SERVER_ERROR; error = e }
+                Response().apply { resultCode = HttpURLConnection.HTTP_INTERNAL_ERROR; error = e }
             }
         }
         return response
     }
 
     override suspend fun getCurrentVacancy(request: SearchRequest): Response {
-        if (isConnected(context) == false) {
-            return Response().apply { resultCode = NO_INTERNET }
+        if (!isConnected(context)) {
+            return Response().apply { resultCode = GlobalConstant.NO_INTERNET }
         }
         val response = withContext(Dispatchers.IO) {
             try {
                 val result = hhService.getVacancy(request.vacancyId)
-                result.apply { resultCode = VALID_RESPONSE }
+                result.apply { resultCode = HttpURLConnection.HTTP_OK }
             } catch (e: HttpException) {
-                Response().apply { resultCode = SERVER_ERROR; error = e }
+                Response().apply { resultCode = HttpURLConnection.HTTP_INTERNAL_ERROR; error = e }
             }
         }
         return response
     }
 
     override suspend fun getSimilarVacancies(request: SearchRequest): Response {
-        if (isConnected(context) == false) {
-            return Response().apply { resultCode = NO_INTERNET }
+        if (!isConnected(context)) {
+            return Response().apply { resultCode = GlobalConstant.NO_INTERNET }
         }
         val response = withContext(Dispatchers.IO) {
             try {
                 val result = hhService.getSimilarVacancies(request.vacancyId, request.searchOptions)
-                result.apply { resultCode = VALID_RESPONSE }
+                result.apply { resultCode = HttpURLConnection.HTTP_OK }
             } catch (e: HttpException) {
-                Response().apply { resultCode = SERVER_ERROR; error = e }
+                Response().apply { resultCode = HttpURLConnection.HTTP_INTERNAL_ERROR; error = e }
             }
         }
         return response
     }
 
     override suspend fun getIndustries(): ResponseGuide<ResponseIndustriesGuideItem> {
-        if (isConnected(context) == false) {
-            return ResponseGuide<ResponseIndustriesGuideItem>().apply { resultCode = NO_INTERNET }
+        if (!isConnected(context)) {
+            return ResponseGuide<ResponseIndustriesGuideItem>().apply { resultCode = GlobalConstant.NO_INTERNET }
         }
         val response = withContext(Dispatchers.IO) {
             try {
                 val resultList = hhService.getIndustries()
                 val result = ResponseGuide<ResponseIndustriesGuideItem>().apply { listItem.addAll(resultList) }
-                result.apply { resultCode = VALID_RESPONSE }
+                result.apply { resultCode = HttpURLConnection.HTTP_OK }
             } catch (e: HttpException) {
-                ResponseGuide<ResponseIndustriesGuideItem>().apply { resultCode = SERVER_ERROR; error = e }
+                ResponseGuide<ResponseIndustriesGuideItem>().apply {
+                    resultCode = HttpURLConnection.HTTP_INTERNAL_ERROR; error = e
+                }
             }
         }
         return response
     }
 
     override suspend fun getAreas(request: GuideRequest): Response {
-        if (isConnected(context) == false) {
-            return Response().apply { resultCode = NO_INTERNET }
+        if (!isConnected(context)) {
+            return Response().apply { resultCode = GlobalConstant.NO_INTERNET }
         }
         val response = withContext(Dispatchers.IO) {
             try {
                 val result = hhService.getAreas(request.id)
-                result.apply { resultCode = VALID_RESPONSE }
+                result.apply { resultCode = HttpURLConnection.HTTP_OK }
             } catch (e: HttpException) {
-                Response().apply { resultCode = SERVER_ERROR; error = e }
+                Response().apply { resultCode = HttpURLConnection.HTTP_INTERNAL_ERROR; error = e }
             }
         }
         return response
     }
 
     override suspend fun getCountries(): ResponseGuide<ResponseCountriesGuideItem> {
-        if (isConnected(context) == false) {
-            return ResponseGuide<ResponseCountriesGuideItem>().apply { resultCode = NO_INTERNET }
+        if (!isConnected(context)) {
+            return ResponseGuide<ResponseCountriesGuideItem>().apply { resultCode = GlobalConstant.NO_INTERNET }
         }
         val response = withContext(Dispatchers.IO) {
             try {
                 val resultList = hhService.getCountries()
                 val result = ResponseGuide<ResponseCountriesGuideItem>().apply { listItem.addAll(resultList) }
-                result.apply { resultCode = VALID_RESPONSE }
+                result.apply { resultCode = HttpURLConnection.HTTP_OK }
             } catch (e: HttpException) {
-                ResponseGuide<ResponseCountriesGuideItem>().apply { resultCode = SERVER_ERROR; error = e }
+                ResponseGuide<ResponseCountriesGuideItem>().apply {
+                    resultCode = HttpURLConnection.HTTP_INTERNAL_ERROR; error = e
+                }
             }
         }
         return response
--- app/src/main/java/ru/practicum/android/diploma/util/GlobalConstant.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/GlobalConstant.kt
@@ -4,4 +4,5 @@ object GlobalConstant {
     const val SETTINGS_APP = "settings_app"
     const val BASE_URL = "https://api.hh.ru"
     const val HH_USER_AGENT: String = "HH-User-Agent: Get a job! (morozov@rtr.spb.ru)"
+    const val NO_INTERNET = -1
 }
