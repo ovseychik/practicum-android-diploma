96ece677192feb2971b3dc586d6ede9f5b8af5e8
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-25 00:09:23 +0300

    - изменил тип данных параметром функций и добавил строковые ресурсы

--- app/src/main/java/ru/practicum/android/diploma/domain/models/SearchResultData.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/SearchResultData.kt
@@ -1,8 +1,10 @@
 package ru.practicum.android.diploma.domain.models
 
+import androidx.annotation.StringRes
+
 sealed interface SearchResultData<T> {
-    data class Data<T>(val value: T) : SearchResultData<T>
-    data class ErrorServer<T>(val message: String) : SearchResultData<T>
-    data class NoInternet<T>(val message: String) : SearchResultData<T>
-    data class Empty<T>(val message: String) : SearchResultData<T>
+    data class Data<T>(val value: T?) : SearchResultData<T>
+    data class ErrorServer<T>(@StringRes val message: Int) : SearchResultData<T>
+    data class NoInternet<T>(@StringRes val message: Int) : SearchResultData<T>
+    data class Empty<T>(@StringRes val message: Int) : SearchResultData<T>
 }
