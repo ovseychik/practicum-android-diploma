1d61fe9f6584467505c3e1db6c22d321f114f4c8
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-25 00:08:38 +0300

    - удалил классы самонаписанных ответов и перевел логику обработки ответа на стандартный класс Result<T>

--- app/src/main/java/ru/practicum/android/diploma/data/NetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/NetworkClient.kt
@@ -1,23 +1,25 @@
 package ru.practicum.android.diploma.data
 
+import ru.practicum.android.diploma.data.dto.DetailsRequest
 import ru.practicum.android.diploma.data.dto.GuideRequest
 import ru.practicum.android.diploma.data.dto.SearchRequest
-import ru.practicum.android.diploma.data.dto.responses.Response
-import ru.practicum.android.diploma.data.dto.responses.ResponseGuide
+import ru.practicum.android.diploma.data.dto.responses.guides.areas.ResponseAreaGuideDto
 import ru.practicum.android.diploma.data.dto.responses.guides.countries.ResponseCountriesGuideItem
 import ru.practicum.android.diploma.data.dto.responses.guides.inustries.ResponseIndustriesGuideItem
+import ru.practicum.android.diploma.data.dto.responses.vacancy.details.ResponseDetailsDto
+import ru.practicum.android.diploma.data.dto.responses.vacancy.list.ResponseListDto
 
 interface NetworkClient {
-    suspend fun getVacancies(request: SearchRequest): Response?
+    suspend fun getVacancies(request: SearchRequest): Result<ResponseListDto>
 
-    suspend fun getCurrentVacancy(request: SearchRequest): Response?
+    suspend fun getCurrentVacancy(request: DetailsRequest): Result<ResponseDetailsDto>
 
-    suspend fun getSimilarVacancies(request: SearchRequest): Response?
+    suspend fun getIndustries(): Result<List<ResponseIndustriesGuideItem>>?
 
-    suspend fun getIndustries(): ResponseGuide<ResponseIndustriesGuideItem>?
+    suspend fun getAreas(request: GuideRequest): Result<ResponseAreaGuideDto>
 
-    suspend fun getAreas(request: GuideRequest): Response?
+    suspend fun getAllAreas(): Result<List<ResponseAreaGuideDto>>
 
-    suspend fun getCountries(): ResponseGuide<ResponseCountriesGuideItem>?
+    suspend fun getCountries(): Result<List<ResponseCountriesGuideItem>>?
 
 }
