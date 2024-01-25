9cefb58ab2314d871c28c2de31b45605c265eb2c
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-25 10:50:00 +0300

    - сделал константы приватными - написал метод получения экземпляра SearchRequest

--- app/src/main/java/ru/practicum/android/diploma/data/dto/SearchRequest.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/SearchRequest.kt
@@ -1,20 +1,37 @@
 package ru.practicum.android.diploma.data.dto
 
+import ru.practicum.android.diploma.data.models.SearchSettings
+
 data class SearchRequest(
-    val searchOptions: Map<String, String> = mutableMapOf(
-        KEY_TEXT to "",
-        KEY_AREA to "",
-        KEY_INDUSTRY to "",
-    ),
-    val onlyWithSalary: Boolean = false,
-    val page: Int,
-    val perPage: Int = ITEMS_PER_SHEET,
-    val salary: Int
+    val searchOptionsStr: Map<String, String>,
+    val searchOptionsNum: Map<String, Int>,
+    val onlyWithSalary: Boolean,
 ) {
     companion object {
-        const val KEY_TEXT = "text"
-        const val KEY_AREA = "area"
-        const val KEY_INDUSTRY = "industry"
-        const val ITEMS_PER_SHEET = 20
+        private const val KEY_TEXT = "text"
+        private const val KEY_AREA = "area"
+        private const val KEY_INDUSTRY = "industry"
+        private const val ITEMS_PER_SHEET = 20
+        private const val KEY_PAGE = "page"
+        private const val KEY_PER_PAGE = "per_page"
+        private const val KEY_SALARY = "salary"
+
+        fun setSearchOptions(
+            text: String,
+            page: Int,
+            searchSettings: SearchSettings
+        ): SearchRequest {
+            val mapStr = mapOf<String, String>(
+                KEY_TEXT to text,
+                KEY_AREA to searchSettings.areaId,
+                KEY_INDUSTRY to searchSettings.industry
+            )
+            val mapNum = mapOf<String, Int>(
+                KEY_PAGE to page,
+                KEY_PER_PAGE to ITEMS_PER_SHEET,
+                KEY_SALARY to searchSettings.salary
+            )
+            return SearchRequest(mapStr, mapNum, searchSettings.isSalarySpecified)
+        }
     }
 }
