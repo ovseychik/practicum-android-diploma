cfc891a49a86c66f73ee51bc0c354a2b3bee6911
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-25 19:55:54 +0300

    - добавил класс Address

---
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/responses/vacancy/details/Address.kt
@@ -0,0 +1,8 @@
+package ru.practicum.android.diploma.data.dto.responses.vacancy.details
+
+data class Address(
+    val building: String,
+    val city: String,
+    val description: String,
+    val street: String
+)
