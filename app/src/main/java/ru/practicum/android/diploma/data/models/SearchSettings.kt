019883fe46aca670fc8913695aafe156bdf5be08
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-25 13:09:00 +0300

    Содал модель класса поисковых параметров

--- app/src/main/java/ru/practicum/android/diploma/data/models/SearchSettings.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/models/SearchSettings.kt
@@ -1,8 +1,11 @@
 package ru.practicum.android.diploma.data.models
 
+const val EMPTY_PARAM_NUM = -1
+const val EMPTY_PARAM_SRT = ""
+
 data class SearchSettings(
-    val salary: Int,
-    val isSalarySpecified: Boolean,
-    val areaId: String,
-    val industry: String
+    var salary: Int = EMPTY_PARAM_NUM,
+    var isSalarySpecified: Boolean = false,
+    var areaId: String = EMPTY_PARAM_SRT,
+    var industry: String = EMPTY_PARAM_SRT
 )
