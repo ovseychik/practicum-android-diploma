aae0be3ed8f53042bd16c481175e89fde0ae3f6d
Mark Quarasique <julipaha@gmail.com>
2024-01-26 17:23:24 +0300

    Добавляет интерфейсы интеракторов

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/VacanciesInteractor.kt
@@ -0,0 +1,9 @@
+package ru.practicum.android.diploma.domain.api
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.models.SearchResultData
+import ru.practicum.android.diploma.domain.models.vacancy.Vacancies
+
+interface VacanciesInteractor {
+    fun getVacancies(query: String, pageNumber: Int): Flow<SearchResultData<Vacancies>>
+}
