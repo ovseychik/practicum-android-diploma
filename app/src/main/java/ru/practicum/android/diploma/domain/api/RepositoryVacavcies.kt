db50ac8f74eb20411470586f3240a90897b9b0e5
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-26 15:24:21 +0300

    написал интерфейсы репозиториев поиска списка вакансий и деталей вакансий

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/RepositoryVacavcies.kt
@@ -0,0 +1,10 @@
+package ru.practicum.android.diploma.domain.api
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.data.dto.SearchRequest
+import ru.practicum.android.diploma.domain.models.SearchResultData
+import ru.practicum.android.diploma.domain.models.vacancy.Vacancies
+
+interface RepositoryVacavcies {
+    suspend fun getVacancies(text: String): Flow<SearchResultData<Vacancies>>
+}
