3442ed424756324efe2114d6495ae910e3cd506a
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-26 23:02:57 +0300

    убрал ошибочно закоммитченое

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -1,9 +1,5 @@
 package ru.practicum.android.diploma.di
 
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-import ru.practicum.android.diploma.presentation.MyViewModel
 
-val viewModelModule = module {
-    viewModel { MyViewModel(repositoryDetails = get(), repositoryVacancies = get()) }
-}
+val viewModelModule = module { }
