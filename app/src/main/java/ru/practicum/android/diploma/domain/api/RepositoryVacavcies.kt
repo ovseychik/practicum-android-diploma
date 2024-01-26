663b17eab2de53e22225bf398e77f6742a275139
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-26 15:43:01 +0300

    форматирование

--- app/src/main/java/ru/practicum/android/diploma/domain/api/RepositoryVacavcies.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/RepositoryVacavcies.kt
@@ -3,7 +3,9 @@ package ru.practicum.android.diploma.domain.api
 import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.domain.models.SearchResultData
 import ru.practicum.android.diploma.domain.models.vacancy.Vacancies
+
 const val START_PAGE = 0
+
 interface RepositoryVacavcies {
     suspend fun getVacancies(text: String, page: Int = START_PAGE): Flow<SearchResultData<Vacancies>>
 }
