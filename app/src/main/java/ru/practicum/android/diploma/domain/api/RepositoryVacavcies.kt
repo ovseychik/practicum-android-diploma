137305d019ec30f0b454be92f884455241a208f1
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-26 15:40:16 +0300

    в RepositoryVacavcies добавил параметр номер страницы

--- app/src/main/java/ru/practicum/android/diploma/domain/api/RepositoryVacavcies.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/RepositoryVacavcies.kt
@@ -3,7 +3,7 @@ package ru.practicum.android.diploma.domain.api
 import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.domain.models.SearchResultData
 import ru.practicum.android.diploma.domain.models.vacancy.Vacancies
-
+const val START_PAGE = 0
 interface RepositoryVacavcies {
-    suspend fun getVacancies(text: String): Flow<SearchResultData<Vacancies>>
+    suspend fun getVacancies(text: String, page: Int = START_PAGE): Flow<SearchResultData<Vacancies>>
 }
