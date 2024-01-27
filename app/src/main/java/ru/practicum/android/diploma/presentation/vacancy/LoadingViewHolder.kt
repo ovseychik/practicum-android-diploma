ffe62a8645043e31fc7390601bba8f8dc0a6b8f3
ovseychiks <ovseychiks@kacd.kz>
2024-01-27 13:48:53 +0600

    Search 45. optimize imports

--- app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/LoadingViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/LoadingViewHolder.kt
@@ -1,10 +1,7 @@
 package ru.practicum.android.diploma.presentation.vacancy
 
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import ru.practicum.android.diploma.R
 
 class LoadingViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     fun bind() {
