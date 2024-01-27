b768d472914cdfd6ef82ce0f3bed2f2078d64077
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-27 11:33:48 +0300

    форматирование

--- app/src/main/java/ru/practicum/android/diploma/presentation/ViewModelVacancies.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/ViewModelVacancies.kt
@@ -6,18 +6,18 @@ import androidx.lifecycle.ViewModel
 import ru.practicum.android.diploma.domain.api.VacanciesInteractor
 import ru.practicum.android.diploma.domain.models.SearchResultData
 import ru.practicum.android.diploma.domain.models.vacancy.Vacancies
-import ru.practicum.android.diploma.presentation.models.VacanciesScreenState
+import ru.practicum.android.diploma.presentation.models.ScreenStateVacancies
 
 class ViewModelVacancies(
     private val vacanciesInteractor: VacanciesInteractor
 ) : ViewModel() {
 
-    private var _screenState: MutableLiveData<VacanciesScreenState> = MutableLiveData()
+    private var _screenState: MutableLiveData<ScreenStateVacancies> = MutableLiveData()
 
-    val screenState: LiveData<VacanciesScreenState> = _screenState
+    val screenState: LiveData<ScreenStateVacancies> = _screenState
     suspend fun getVacancies(query: String, pageNum: Int = 0) {
         if (query.isNotEmpty()) {
-            _screenState.postValue(VacanciesScreenState.IsLoading)
+            _screenState.postValue(ScreenStateVacancies.IsLoading)
             vacanciesInteractor.getVacancies(query, pageNum).collect { result ->
                 processingResult(result)
             }
@@ -27,20 +27,20 @@ class ViewModelVacancies(
     private fun processingResult(result: SearchResultData<Vacancies>) {
         when (result) {
             is SearchResultData.NoInternet -> {
-                _screenState.postValue(VacanciesScreenState.NoInternet(result.message))
+                _screenState.postValue(ScreenStateVacancies.NoInternet(result.message))
             }
 
             is SearchResultData.ErrorServer -> {
-                _screenState.postValue(VacanciesScreenState.NoInternet(result.message))
+                _screenState.postValue(ScreenStateVacancies.NoInternet(result.message))
             }
 
             is SearchResultData.Empty -> {
-                _screenState.postValue(VacanciesScreenState.NoInternet(result.message))
+                _screenState.postValue(ScreenStateVacancies.NoInternet(result.message))
             }
 
             is SearchResultData.Data -> {
                 _screenState.postValue(
-                    VacanciesScreenState.Content(
+                    ScreenStateVacancies.Content(
                         result.value?.foundItems!!,
                         result.value.listVacancies
                     )
