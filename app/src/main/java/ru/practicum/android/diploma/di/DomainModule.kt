d10dbdb90ce947eaa14a355100f314f9c23ab235
Mark Quarasique <julipaha@gmail.com>
2024-01-27 10:53:36 +0300

    Добавляет реализацию интерактора фичи детали

--- app/src/main/java/ru/practicum/android/diploma/di/DomainModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DomainModule.kt
@@ -3,8 +3,10 @@ package ru.practicum.android.diploma.di
 import org.koin.dsl.module
 import ru.practicum.android.diploma.data.network.RepositoryDetailsImpl
 import ru.practicum.android.diploma.data.network.RepositoryVacanciesImpl
+import ru.practicum.android.diploma.domain.api.DetailsInteractor
 import ru.practicum.android.diploma.domain.api.RepositoryDetails
 import ru.practicum.android.diploma.domain.api.RepositoryVacancies
+import ru.practicum.android.diploma.domain.impl.details.DetailsInteractorImpl
 
 val domainModule = module {
     single<RepositoryDetails> {
@@ -13,5 +15,9 @@ val domainModule = module {
 
     single<RepositoryVacancies> {
         RepositoryVacanciesImpl(client = get(), settingsPref = get(), json = get())
+    }
+
+    single<DetailsInteractor> {
+        DetailsInteractorImpl(detailsRepository = get())
     }
 }
