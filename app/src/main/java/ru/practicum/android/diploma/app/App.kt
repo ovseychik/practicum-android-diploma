9e78eaa06385efe387151540a3902fd960eb1139
Aslan <shkahov91@gmail.com>
2024-01-28 16:41:46 +0300

    добавил модуль для репозиториев в App

--- app/src/main/java/ru/practicum/android/diploma/app/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/app/App.kt
@@ -5,6 +5,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import ru.practicum.android.diploma.di.dataModule
 import ru.practicum.android.diploma.di.domainModule
+import ru.practicum.android.diploma.di.repositoryModule
 import ru.practicum.android.diploma.di.viewModelModule
 
 class App : Application() {
@@ -13,7 +14,7 @@ class App : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@App)
-            modules(dataModule, domainModule, viewModelModule)
+            modules(dataModule, domainModule, viewModelModule, repositoryModule)
         }
     }
 }
