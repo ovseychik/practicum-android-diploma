f0610a90f2253278420327a66354c4915f147aab
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-28 19:16:51 +0300

    - внес зависимости в di

--- app/src/main/java/ru/practicum/android/diploma/di/DomainModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DomainModule.kt
@@ -1,13 +1,16 @@
 package ru.practicum.android.diploma.di
 
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.ExternalNavigatorImpl
 import ru.practicum.android.diploma.data.network.RepositoryDetailsImpl
 import ru.practicum.android.diploma.data.network.RepositoryVacanciesImpl
 import ru.practicum.android.diploma.domain.api.DetailsInteractor
+import ru.practicum.android.diploma.domain.api.ExternalNavigator
 import ru.practicum.android.diploma.domain.api.RepositoryDetails
 import ru.practicum.android.diploma.domain.api.RepositoryVacancies
-import ru.practicum.android.diploma.domain.impl.details.DetailsInteractorImpl
 import ru.practicum.android.diploma.domain.api.VacanciesInteractor
+import ru.practicum.android.diploma.domain.impl.details.DetailsInteractorImpl
 import ru.practicum.android.diploma.domain.impl.search.VacanciesInteractorImpl
 
 val domainModule = module {
@@ -24,5 +27,9 @@ val domainModule = module {
 
     single<VacanciesInteractor> {
         VacanciesInteractorImpl(vacanciesRepository = get())
+    }
+
+    single<ExternalNavigator> {
+        ExternalNavigatorImpl(androidContext())
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -2,10 +2,15 @@ package ru.practicum.android.diploma.di
 
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import ru.practicum.android.diploma.presentation.vacancy.viewmodel.DetailsViewModel
 import ru.practicum.android.diploma.presentation.vacancy.viewmodel.SearhViewModel
 
 val viewModelModule = module {
     viewModel {
         SearhViewModel(vacanciesInteractor = get())
+    }
+
+    viewModel<DetailsViewModel> {
+        DetailsViewModel(detailsInteractor = get(), externalNavigator = get())
     }
 }
