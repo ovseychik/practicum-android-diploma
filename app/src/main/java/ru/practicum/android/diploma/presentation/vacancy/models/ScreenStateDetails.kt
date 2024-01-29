a514f271a5e50debbcbc7d77b1d10f26caf10bb7
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-29 17:39:16 +0300

    добавил state для окончательного показа нет интернета

--- app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/models/ScreenStateDetails.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/models/ScreenStateDetails.kt
@@ -8,4 +8,6 @@ sealed interface ScreenStateDetails {
     data class Content(val details: VacancyDetails) : ScreenStateDetails
     data class Error(@StringRes val message: Int) : ScreenStateDetails
     data class NoInternet(@StringRes val message: Int) : ScreenStateDetails
+
+    object NoVacansyFromDb: ScreenStateDetails
 }
