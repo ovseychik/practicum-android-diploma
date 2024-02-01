7e9ddd92b0d44e0c1ded1d7dac3675bf80731659
AlexandrKorytin <al.corytin@yandex.ru>
2024-02-01 20:21:39 +0300

    - написал интерфейс и реализацию репозитория индустрий

---
+++ app/src/main/java/ru/practicum/android/diploma/data/network/guide/IndustriesRepositoryImpl.kt
@@ -0,0 +1,38 @@
+package ru.practicum.android.diploma.data.network.guide
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.data.NetworkClient
+import ru.practicum.android.diploma.data.dto.responses.guides.inustries.mapToListIndustriesItem
+import ru.practicum.android.diploma.domain.api.guides.IndustriesRepository
+import ru.practicum.android.diploma.domain.models.SearchResultData
+import ru.practicum.android.diploma.domain.models.guides.IndustryItem
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+
+class IndustriesRepositoryImpl(private val client: NetworkClient) : IndustriesRepository {
+    override suspend fun getIndustries(): Flow<SearchResultData<List<IndustryItem>>> = flow {
+        val searchResult = client.getIndustries()
+        val data = searchResult?.getOrNull()
+        val error = searchResult?.exceptionOrNull()
+        when {
+            data != null -> {
+                emit(SearchResultData.Data(mapToListIndustriesItem(data)))
+            }
+
+            error is ConnectException -> {
+                emit(SearchResultData.NoInternet(R.string.no_internet))
+            }
+
+            error is SocketTimeoutException -> {
+                emit(SearchResultData.NoInternet(R.string.no_internet))
+            }
+
+            error is HttpException -> {
+                emit(SearchResultData.ErrorServer(R.string.server_error))
+            }
+        }
+    }
+}
