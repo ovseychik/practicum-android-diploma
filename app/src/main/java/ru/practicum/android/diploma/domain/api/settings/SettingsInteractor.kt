f104010ff888085ee44967c343a2bb1c9021d11f
AlexandrKorytin <al.corytin@yandex.ru>
2024-02-02 09:26:21 +0300

    - форматирование - написан интерфейс интерактора экрана выбора индустрии и его реализация

--- app/src/main/java/ru/practicum/android/diploma/domain/api/settings/SettingsInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/settings/SettingsInteractor.kt
@@ -1,8 +1,5 @@
 package ru.practicum.android.diploma.domain.api.settings
 
-import ru.practicum.android.diploma.domain.models.guides.Country
-import ru.practicum.android.diploma.domain.models.guides.IndustryItem
-import ru.practicum.android.diploma.domain.models.guides.PlaceItem
 import ru.practicum.android.diploma.domain.models.settings.SearchSettings
 
 interface SettingsInteractor {
--- app/src/main/java/ru/practicum/android/diploma/domain/impl/settings/SettingInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/impl/settings/SettingInteractorImpl.kt
@@ -4,9 +4,9 @@ import ru.practicum.android.diploma.domain.api.settings.SettingsInteractor
 import ru.practicum.android.diploma.domain.api.settings.SettingsRepository
 import ru.practicum.android.diploma.domain.models.settings.SearchSettings
 
-class SettingInteractorImpl(private val settingsRepository: SettingsRepository): SettingsInteractor {
+class SettingInteractorImpl(private val settingsRepository: SettingsRepository) : SettingsInteractor {
     override fun getSettings(): SearchSettings {
-        return  settingsRepository.getSettings()
+        return settingsRepository.getSettings()
     }
 
     override fun saveSettings(settings: SearchSettings) {
