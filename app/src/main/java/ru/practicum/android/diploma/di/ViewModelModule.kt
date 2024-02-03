41ae2b4ca45ba9b91cdc806526b58c71692f38fc
Mark Quarasique <julipaha@gmail.com>
2024-02-03 17:07:42 +0300

    Переносит вью модель в соответствующий пакет

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -3,7 +3,7 @@ package ru.practicum.android.diploma.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.presentation.favorite.viewmodel.FavoriteViewModel
-import ru.practicum.android.diploma.presentation.settings.viewmodel.LocalityTypeViewModel
+import ru.practicum.android.diploma.presentation.settings.viewmodels.LocalityTypeViewModel
 import ru.practicum.android.diploma.presentation.settings.viewmodels.IndustriesViewModel
 import ru.practicum.android.diploma.presentation.settings.viewmodels.SettingsViewModel
 import ru.practicum.android.diploma.presentation.vacancy.viewmodel.DetailsViewModel
--- app/src/main/java/ru/practicum/android/diploma/presentation/settings/viewmodels/LocalityTypeViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/settings/viewmodels/LocalityTypeViewModel.kt
@@ -1,4 +1,4 @@
-package ru.practicum.android.diploma.presentation.settings.viewmodel
+package ru.practicum.android.diploma.presentation.settings.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
