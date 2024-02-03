a7aa3940c35f9a27daead75b8497a10f7ee993dc
Mark Quarasique <julipaha@gmail.com>
2024-02-03 15:26:48 +0300

    Добавляет вью модель для фрагмента выбора места работы

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/settings/viewmodel/LocalityTypeViewModel.kt
@@ -0,0 +1,45 @@
+package ru.practicum.android.diploma.presentation.settings.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.domain.api.guides.PlacesInteractor
+import ru.practicum.android.diploma.domain.models.SearchResultData
+import ru.practicum.android.diploma.domain.models.guides.Country
+import ru.practicum.android.diploma.domain.models.guides.PlaceItem
+import ru.practicum.android.diploma.presentation.settings.models.LocalityTypeScreenState
+
+class LocalityTypeViewModel(private val placesInteractor: PlacesInteractor) : ViewModel() {
+
+    private var place: PlaceItem = placesInteractor.getPlaceFromSettings()
+    private var country: Country = placesInteractor.getCountryFromSettings()
+    private val _screenState: MutableLiveData<LocalityTypeScreenState> = MutableLiveData()
+    val screenState: LiveData<LocalityTypeScreenState> = _screenState
+
+    init {
+        setCountryByPlaceId(place)
+    }
+
+    private fun setCountryByPlaceId(placeItem: PlaceItem) {
+        if (country.countryId.isEmpty() && placeItem.areaId.isEmpty()) {
+            viewModelScope.launch(Dispatchers.IO) {
+                placesInteractor.getCountryById(placeItem.areaId).collect {
+                    if (it is SearchResultData.Data) {
+                        country = it.value!!
+                    }
+                }
+            }
+        }
+    }
+
+    fun updateState() {
+        if (place.areaId.isNotEmpty()) {
+            _screenState.postValue(LocalityTypeScreenState.Content(place, country))
+        } else {
+            _screenState.postValue(LocalityTypeScreenState.Empty)
+        }
+    }
+}
