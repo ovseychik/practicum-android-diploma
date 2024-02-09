c4250044fa15fc67ed4039a016291cf7b62f2968
AlexandrKorytin <al.corytin@yandex.ru>
2024-02-09 10:01:47 +0300

    - добавил анимации - применил анамацию на экране о команде

--- app/src/main/java/ru/practicum/android/diploma/ui/team/fragment/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/team/fragment/TeamFragment.kt
@@ -1,12 +1,29 @@
 package ru.practicum.android.diploma.ui.team.fragment
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentTeamBinding
 import ru.practicum.android.diploma.util.BindingFragment
 
 class TeamFragment : BindingFragment<FragmentTeamBinding>() {
     override fun createBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentTeamBinding {
         return FragmentTeamBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val inAnim = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_in_bottom)
+        lifecycleScope.launch {
+            binding.teamMember1.startAnimation(inAnim)
+            binding.teamMember2.startAnimation(inAnim)
+            binding.teamMember3.startAnimation(inAnim)
+            binding.teamMember4.startAnimation(inAnim)
+        }
     }
 }
